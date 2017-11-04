55e2c96fe6c846bd51d82f6df59d0ba5b91b6661
leeway-deng <13146209849@163.com>
2017-11-04 23:57:38 +0800

    +redis pool manager

--- src/common/cache/redis/pool_manager.go
+++ src/common/cache/redis/pool_manager.go
@@ -2,9 +2,9 @@ package redis
 
 import (
 	"time"
-	"os/exec"
 	"github.com/garyburd/redigo/redis"
 	"sync"
+	"fmt"
 )
 
 var (
@@ -15,23 +15,55 @@ var (
 type RedisPoolConfig struct {
 	host string
 	port  int
+	password string
 	maxIdle int
 	maxActive int
 	idleTimeout time.Duration
+	readTimeout time.Duration
+	writeTimeout time.Duration
 	isTestOnBorrow bool
 }
 
 func NewPool(poolConfig RedisPoolConfig, args ...string) (*redis.Pool, error) {
-	s := &Server{
-		name: name,
-		cmd:  exec.Command(*serverPath, args...),
-		done: make(chan struct{}),
+	if (redisPool == nil) {
+		redisPool = &redis.Pool {
+			// Other pool configuration not shown in this example.
+			Dial: func() (redis.Conn, error) {
+				var addr string
+				if (poolConfig.port > 0) {
+					addr = fmt.Sprintf("%s:%d", poolConfig.host, poolConfig.port)
+				} else {
+					addr = poolConfig.host
+				}
+				c, err := redis.Dial("tcp", addr,
+					redis.DialReadTimeout(poolConfig.readTimeout),
+					redis.DialWriteTimeout(poolConfig.writeTimeout))
+				if err != nil {
+					return nil, err
+				}
+				if (poolConfig.password != "") {
+					if _, err := c.Do("AUTH", poolConfig.password); err != nil {
+						c.Close()
+						return nil, err
+					}
+				}
+				return c, nil
+			},
+			MaxIdle: poolConfig.maxIdle,
+			MaxActive: poolConfig.maxActive,
+			IdleTimeout: poolConfig.idleTimeout,
+		}
+
+		if (poolConfig.isTestOnBorrow) {
+			redisPool.TestOnBorrow = func(c redis.Conn, t time.Time) error {
+				if time.Since(t) < time.Minute {
+					return nil
+				}
+				_, err := c.Do("PING")
+				return err
+			}
+		}
 	}
 
-	if err != nil {
-		s.Stop()
-		return nil, err
-	}
-
-	return s, nil
+	return redisPool, nil
 }
