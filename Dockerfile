c36bfd52630a691c260665ed40f16214271e440f
leeway-deng <13146209849@163.com>
2018-03-21 23:30:44 +0800

    to fix the mounted host volume for container

--- Dockerfile
+++ Dockerfile
@@ -13,15 +13,15 @@ MAINTAINER dengliwei dengliwei@le.com
 EXPOSE 8081
 
 RUN mkdir -p /app/go/leoGo/config
+RUN mkdir -p /data/leoGo
 
 COPY dist/leoGo-amd64-1.0.0/leoGo /app/go/leoGo
 COPY config /app/go/leoGo/config
 
 WORKDIR /app/go
-#ENTRYPOINT /app/go/leoGo
-
 VOLUME /data/leoGo
 
+#ENTRYPOINT /app/go/leoGo/leoGo
 #RUN ["chmod", "+x", "/app/go/leoGo/leoGo"]
 #CMD [ "/bin/bash", "-c", "ls -ll /app/go/leoGo" ]
 #CMD [ "/bin/bash", "-c", "go version" ]
--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#进入容器：docker exec -it [cid] /bin/bash
 #项目容器名
 prj_name=leoGo
 #项目镜像地址
@@ -15,7 +16,7 @@ docker images|grep none|awk '{print $3 }'|xargs docker rmi
 if docker stop $prj_name; then docker rm $prj_name; fi
 
 #基于prj_image镜像后台运行prj_name容器.
-docker run -d -p $prj_port_host:$prj_port_container --name $prj_name $prj_image
+docker run -d -v /data/leoGo:/data/leoGo -p $prj_port_host:$prj_port_container --name $prj_name $prj_image
 
 #服务器上删除任何没有用的image,持服务器整洁并降低磁盘空间的占用.
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
